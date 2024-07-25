73e38a71a4b52bec1e2cfb86d193ee8d4194c98e
Rudy van den Brink <30622165+rudyvdbrink@users.noreply.github.com>
2024-07-25 16:15:04 +0200

    added SQL
    added SQL skeleton script
    added __name__ dependency in load_data.py
    expanded the example

--- load_data.py
+++ load_data.py
@@ -3,39 +3,40 @@ import os
 from sklearn.datasets import load_iris
 import seaborn as sns
 
-if not os.path.exists("./data"):
-    os.mkdir("data")
+if __name__ == "__main__":
+    if not os.path.exists("./data"):
+        os.mkdir("data")
 
-#get iris dataset
-iris = load_iris()
+    #get iris dataset
+    iris = load_iris()
 
-df = pd.DataFrame(
-    iris.data, 
-    columns=iris.feature_names
-    )
+    df = pd.DataFrame(
+        iris.data, 
+        columns=iris.feature_names
+        )
 
-df['target'] = iris.target
+    df['target'] = iris.target
 
-# Map targets to target names
-target_names = {
-    0:'setosa',
-    1:'versicolor', 
-    2:'virginica'
-}
+    # Map targets to target names
+    target_names = {
+        0:'setosa',
+        1:'versicolor', 
+        2:'virginica'
+    }
 
-df['target_names'] = df['target'].map(target_names)
+    df['target_names'] = df['target'].map(target_names)
 
-#clean up column names
-df.columns = [col.replace(' ', '_') for col in df.columns]
-df.columns = [col.replace('(',  '') for col in df.columns]
-df.columns = [col.replace(')',  '') for col in df.columns]
+    #clean up column names
+    df.columns = [col.replace(' ', '_') for col in df.columns]
+    df.columns = [col.replace('(',  '') for col in df.columns]
+    df.columns = [col.replace(')',  '') for col in df.columns]
 
-df.to_csv('data/data_iris.csv', sep='\t',index=False)
-print("Iris data saved successfully")
+    df.to_csv('data/data_iris.csv', sep='\t',index=False)
+    print("Iris data saved successfully")
 
-#get titanic dataset
-df = sns.load_dataset('titanic')
-df.to_csv('data/data_titanic.csv', sep='\t',index=False)
-print("Titanic data saved successfully")
+    #get titanic dataset
+    df = sns.load_dataset('titanic')
+    df.to_csv('data/data_titanic.csv', sep='\t',index=False)
+    print("Titanic data saved successfully")
 
-exit()
+    exit()
